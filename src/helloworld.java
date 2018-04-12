799f22698dc9bf19a8863ab17edee4aff4963723
Minami Okuno <m.3.pguitar@gmail.com>
2018-04-12 14:15:32 +0900

    helloworldの文字列変更

--- src/helloworld.java
+++ src/helloworld.java
@@ -3,7 +3,7 @@ public class helloworld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("1年ぶりのプログラミング！");
+		System.out.println("1年ぶりのプログラミングだったのですが、何も覚えていなくて驚愕しました。精一杯頑張ります！");
 	}
 
 }
